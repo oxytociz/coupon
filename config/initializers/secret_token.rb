fbc504e401429380b9f25c8ec467ef2da8601858
Dekpump <sakarin@webwarestudios.com>
2010-12-10 23:12:08 +0700

    Init commit coupon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coupon::Application.config.secret_token = '152956fa7cc78237ad175c0becfa08a76a27be457a834241d294956ee6968e8b87eae393768be3424a10afb4ee52c068bb31e18b366ee1e45a3c1698f824c4b6'
